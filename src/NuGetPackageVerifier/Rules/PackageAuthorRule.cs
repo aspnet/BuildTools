6783f4692e2900c405c2a1ba7b3489c43cc99cfd
Ryan Brandenburg <rybrande@microsoft.com>
2016-06-29 12:22:58 -0700

    Add an author checking rule (#73)

---
+++ src/NuGetPackageVerifier/Rules/PackageAuthorRule.cs
@@ -0,0 +1,37 @@
+﻿// Copyright (c) .NET Foundation. All rights reserved.
+// Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
+
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using NuGet.Packaging;
+using NuGetPackageVerifier.Logging;
+
+namespace NuGetPackageVerifier.Rules
+{
+    public class PackageAuthorRule : IPackageVerifierRule
+    {
+        private const string _expectedAuthor = "Microsoft";
+
+        public IEnumerable<PackageVerifierIssue> Validate(
+            FileInfo nupkgFile,
+            IPackageMetadata package,
+            IPackageVerifierLogger logger)
+        {
+            if (package.Authors == null || package.Authors.Count() < 1)
+            {
+                yield return PackageIssueFactory.RequiredAuthor();
+            }
+
+            if (package.Authors.Count() > 1)
+            {
+                yield return PackageIssueFactory.SingleAuthorOnly(package.Id);
+            }
+
+            if (string.Equals(package.Authors.First(), _expectedAuthor, System.StringComparison.Ordinal))
+            {
+                yield return PackageIssueFactory.AuthorIsIncorrect(package.Id, _expectedAuthor);
+            }
+        }
+    }
+}

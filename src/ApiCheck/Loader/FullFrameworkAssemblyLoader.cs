e0ee27a91dee20ea92191e1328d60f59838010ee
jacalvar <jacalvar@microsoft.com>
2016-10-18 16:20:44 -0700

    Added license headers

--- src/ApiCheck/Loader/FullFrameworkAssemblyLoader.cs
+++ src/ApiCheck/Loader/FullFrameworkAssemblyLoader.cs
@@ -1,4 +1,7 @@
-﻿#if !NETCOREAPP1_0
+﻿// Copyright (c) .NET Foundation. All rights reserved.
+// Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
+
+#if !NETCOREAPP1_0
 
 using System;
 using System.Collections.Generic;

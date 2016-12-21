8efd56408aacbb43f999cf278b09a964cd1a0913
Ajay Bhargav Baaskaran <ajbaaska@microsoft.com>
2016-12-21 15:43:41 -0800

    Removed unnecessary Console.WriteLine()

--- src/NuGetPackageVerifier/Program.cs
+++ src/NuGetPackageVerifier/Program.cs
@@ -194,8 +194,6 @@ namespace NuGetPackageVerifier
                             totalErrors += packageErrorsAndWarnings.Item1;
                             totalWarnings += packageErrorsAndWarnings.Item2;
 
-                            Console.WriteLine();
-
                             processedPackages.Add(package);
                         }
                     }

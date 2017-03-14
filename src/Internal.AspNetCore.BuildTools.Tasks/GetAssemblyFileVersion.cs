5b25269bdf4d8a9d6019046a5746f95b1cb1fa6e
Pranav K <prkrishn@hotmail.com>
2017-03-13 17:01:52 -0700

    Set AssemblyFileVersion on generated assemblies
    Fixes #188

---
+++ src/Internal.AspNetCore.BuildTools.Tasks/GetAssemblyFileVersion.cs
@@ -0,0 +1,43 @@
+﻿// Copyright (c) .NET Foundation. All rights reserved.
+// Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
+
+using System;
+using Microsoft.Build.Framework;
+using Microsoft.Build.Utilities;
+
+namespace Microsoft.AspNetCore.BuildTools
+{
+    public class GetAssemblyFileVersion : Task
+    {
+        [Required]
+        public string AssemblyVersion { get; set; }
+
+        [Required]
+        public int AssemblyRevision { get; set; }
+
+        [Output]
+        public string AssemblyFileVersion { get; set; }
+
+        public override bool Execute()
+        {
+            if (!Version.TryParse(AssemblyVersion, out var assemblyVersionValue))
+            {
+                Log.LogError("Invalid value '{0}' for {1}.", AssemblyVersion, nameof(AssemblyVersion));
+                return false;
+            }
+
+            var assemblyFileVersionValue = assemblyVersionValue;
+            if (assemblyFileVersionValue.Revision <= 0)
+            {
+                assemblyFileVersionValue = new Version(
+                    assemblyFileVersionValue.Major,
+                    assemblyFileVersionValue.Minor,
+                    assemblyFileVersionValue.Build,
+                    AssemblyRevision);
+            }
+
+            AssemblyFileVersion = assemblyFileVersionValue.ToString();
+            return true;
+        }
+    }
+}

c135c3bcc9754773f1e9fc527d847f8f4b247e1c
Pranav K <prkrishn@hotmail.com>
2017-03-13 17:01:52 -0700

    Set AssemblyFileVersion on generated assemblies
    Fixes #188

---
+++ test/BuildTools.Tasks.Tests/GetAssemblyFileVersionTest.cs
@@ -0,0 +1,48 @@
+﻿// Copyright (c) .NET Foundation. All rights reserved.
+// Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
+
+using Xunit;
+
+namespace Microsoft.AspNetCore.BuildTools
+{
+    public class GetAssemblyFileVersionTest
+    {
+        [Fact]
+        public void Execute_SetsAssemblyFileVersionToAssemblyVersion_IfRevisionIsSet()
+        {
+            // Arrange
+            var getAssemblyFileVersion = new GetAssemblyFileVersion
+            {
+                AssemblyVersion = "1.2.3.4",
+                AssemblyRevision = 78,
+            };
+
+            // Act
+            var result = getAssemblyFileVersion.Execute();
+
+            // Assert
+            Assert.True(result);
+            Assert.Equal("1.2.3.4", getAssemblyFileVersion.AssemblyFileVersion);
+        }
+
+        [Theory]
+        [InlineData("1.2.3")]
+        [InlineData("1.2.3.0")]
+        public void Execute_UsesAssemblyRevision_IfRevisionIsNotSet(string assemblyVersion)
+        {
+            // Arrange
+            var getAssemblyFileVersion = new GetAssemblyFileVersion
+            {
+                AssemblyVersion = assemblyVersion,
+                AssemblyRevision = 78,
+            };
+
+            // Act
+            var result = getAssemblyFileVersion.Execute();
+
+            // Assert
+            Assert.True(result);
+            Assert.Equal("1.2.3.78", getAssemblyFileVersion.AssemblyFileVersion);
+        }
+    }
+}

dc9a0755cf177faf8a1a170d4567cb328d057e8f
Kiran Challa <kichalla@microsoft.com>
2017-03-23 11:58:53 -0700

    Updated PackagePublisher tool

--- src/PackagePublisher/Program.cs
+++ src/PackagePublisher/Program.cs
@@ -19,10 +19,8 @@ namespace PackagePublisher
     {
         private const string ApiKeyEnvironmentVariableName = "APIKey";
         private static CommandOption _packagesDirectory;
-        private static CommandOption _packagePath;
         private static CommandOption _feedToUploadTo;
         private static CommandOption _maxRetryCountOption;
-        private static IEnumerable<PackageInfo> _packages;
 
         // Do NOT log the key
         private static string _apiKey = null;
@@ -35,10 +33,6 @@ namespace PackagePublisher
                 "-d|--directory",
                 "The directory containing .nupkg file(s)",
                 CommandOptionType.SingleValue);
-            _packagePath = app.Option(
-                "-p|--package",
-                "Full file path to a .nupkg file",
-                CommandOptionType.MultipleValue);
             _feedToUploadTo = app.Option(
                 "-f|--feed",
                 "Feed to upload the .nupkg file(s) to",
@@ -63,50 +57,29 @@ namespace PackagePublisher
                     return 1;
                 }
 
-                var packageDir = _packagesDirectory.Value();
-                if (!string.IsNullOrEmpty(packageDir))
-                {
-                    if (!Directory.Exists(packageDir))
+                var packages = new DirectoryInfo(_packagesDirectory.Value())
+                    .EnumerateFiles("*.nupkg", SearchOption.AllDirectories)
+                    .Where(fileInfo => !fileInfo.Name.EndsWith(".symbols.nupkg", StringComparison.OrdinalIgnoreCase))
+                    .Select(fileInfo =>
                     {
-                        Log.WriteError($"The supplied directory '{packageDir}' does not exist.");
-                        return 1;
-                    }
-                    else
-                    {
-                        _packages = new DirectoryInfo(packageDir)
-                        .EnumerateFiles("*.nupkg", SearchOption.AllDirectories)
-                        .ProcessPackages();
-                    }
-                }
-                else
-                {
-                    var files = new List<FileInfo>();
-                    foreach (var filePath in _packagePath.Values)
-                    {
-                        if (!File.Exists(filePath))
+                        using (var fileStream = File.OpenRead(fileInfo.FullName))
+                        using (var reader = new PackageArchiveReader(fileStream))
                         {
-                            Log.WriteError($"The supplied file '{filePath}' does not exist.");
-                            return 1;
+                            return new PackageInfo()
+                            {
+                                Identity = reader.GetIdentity(),
+                                PackagePath = fileInfo.FullName
+                            };
                         }
-                        var fileInfo = new FileInfo(filePath);
-                        if (!string.Equals(fileInfo.Extension, ".nupkg", StringComparison.OrdinalIgnoreCase))
-                        {
-                            Log.WriteError($"The supplied file '{filePath}' is not a 'nupkg' file.");
-                            return 1;
-                        }
-                        files.Add(fileInfo);
-                    }
+                    });
 
-                    _packages = files.ProcessPackages();
-                }
-
-                if (_packages.Count() == 0)
+                if (packages.Count() == 0)
                 {
-                    Log.WriteWarning("No package files were found to be published!!");
+                    Console.WriteLine("No package files were found to be published!!");
                     return 1;
                 }
 
-                PublishToFeedAsync().Wait();
+                PublishToFeedAsync(packages).Wait();
 
                 return 0;
             });
@@ -117,50 +90,40 @@ namespace PackagePublisher
             }
             catch (Exception ex)
             {
-                Log.WriteError(ex.ToString());
+                Console.WriteLine(ex);
                 return 1;
             }
         }
 
         private static bool AreValidParameters()
         {
-            if (!_feedToUploadTo.HasValue() || string.IsNullOrEmpty(_feedToUploadTo.Value()))
+            if (!_feedToUploadTo.HasValue())
             {
-                Log.WriteError("Feed is required.");
+                Console.WriteLine("Feed is required.");
                 return false;
+            }
+
+            if (!_packagesDirectory.HasValue())
+            {
+                Console.WriteLine("Packages directory is required");
+                return false;
+            }
+            else
+            {
+                var packageDir = _packagesDirectory.Value();
+                if (!Directory.Exists(packageDir))
+                {
+                    Console.WriteLine($"The supplied directory '{packageDir}' does not exist.");
+                    return false;
+                }
             }
 
             _apiKey = Environment.GetEnvironmentVariable(ApiKeyEnvironmentVariableName);
             if (string.IsNullOrEmpty(_apiKey))
             {
-                Log.WriteError("Api key is required to publish to the feed. Set the environment " +
-                    $"variable {ApiKeyEnvironmentVariableName} before inovking this tool again.");
+                Console.WriteLine("Api key is required to publish to the feed. Set the environment " +
+                    $"variable {ApiKeyEnvironmentVariableName} before invoking this tool again.");
                 return false;
-            }
-
-            if (!_packagesDirectory.HasValue() && !_packagePath.HasValue())
-            {
-                Log.WriteError("Provide a directory path with the packages or a full file path to package(s)");
-                return false;
-            }
-            else if (_packagesDirectory.HasValue() && _packagePath.HasValue())
-            {
-                Log.WriteError("Both package directory and package path cannot be supplied. Specify either of them only.");
-                return false;
-            }
-            else
-            {
-                if (_packagesDirectory.HasValue() && string.IsNullOrEmpty(_packagesDirectory.Value()))
-                {
-                    Log.WriteError("Invalid value for package directory.");
-                    return false;
-                }
-
-                if (_packagePath.HasValue() && _packagePath.Values.Count == 0)
-                {
-                    Log.WriteError("Invalid value for package path.");
-                    return false;
-                }
             }
 
             if (_maxRetryCountOption.HasValue())
@@ -171,24 +134,7 @@ namespace PackagePublisher
             return true;
         }
 
-        private static IEnumerable<PackageInfo> ProcessPackages(this IEnumerable<FileInfo> packageFiles)
-        {
-            return packageFiles
-                .Select(fileInfo =>
-                {
-                    using (var fileStream = File.OpenRead(fileInfo.FullName))
-                    using (var reader = new PackageArchiveReader(fileStream))
-                    {
-                        return new PackageInfo()
-                        {
-                            Identity = reader.GetIdentity(),
-                            PackagePath = fileInfo.FullName
-                        };
-                    }
-                });
-        }
-
-        private static async Task PublishToFeedAsync()
+        private static async Task PublishToFeedAsync(IEnumerable<PackageInfo> packages)
         {
             using (var semaphore = new SemaphoreSlim(initialCount: 4, maxCount: 4))
             {
@@ -196,7 +142,7 @@ namespace PackagePublisher
                 var metadataResource = await sourceRepository.GetResourceAsync<MetadataResource>();
                 var packageUpdateResource = await sourceRepository.GetResourceAsync<PackageUpdateResource>();
 
-                var tasks = _packages.Select(async package =>
+                var tasks = packages.Select(async package =>
                 {
                     await semaphore.WaitAsync(TimeSpan.FromMinutes(3));
                     try
@@ -218,7 +164,7 @@ namespace PackagePublisher
             while (true)
             {
                 attempt++;
-                Log.WriteInformation($"Attempting to publish package {package.Identity} (Attempt: {attempt})");
+                Console.WriteLine($"Attempting to publish package {package.Identity} (Attempt: {attempt})");
 
                 try
                 {
@@ -231,14 +177,14 @@ namespace PackagePublisher
                         getSymbolApiKey: _ => null,
                         log: NullLogger.Instance);
 
-                    Log.WriteInformation($"Done publishing package {package.Identity}");
+                    Console.WriteLine($"Done publishing package {package.Identity}");
                     return;
                 }
                 catch (Exception ex) when (attempt <= _maxRetryCount)
                 {
-                    Log.WriteInformation($"Attempt {attempt} failed to publish package {package.Identity}.");
-                    Log.WriteInformation(ex.ToString());
-                    Log.WriteInformation("Retrying...");
+                    Console.WriteLine($"Attempt {attempt} failed to publish package {package.Identity}.");
+                    Console.WriteLine(ex.ToString());
+                    Console.WriteLine("Retrying...");
                 }
             }
         }

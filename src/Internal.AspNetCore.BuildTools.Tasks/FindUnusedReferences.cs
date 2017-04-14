d2a8f8cdc6edf629b879d811667a8cdb0f2d9622
Pavel Krymets <pavel@krymets.com>
2017-04-14 09:44:30 -0700

    Nits

--- src/Internal.AspNetCore.BuildTools.Tasks/FindUnusedReferences.cs
+++ src/Internal.AspNetCore.BuildTools.Tasks/FindUnusedReferences.cs
@@ -1,10 +1,12 @@
-﻿using System;
+﻿// Copyright (c) .NET Foundation. All rights reserved.
+// Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
+
+using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Reflection.Metadata;
 using System.Reflection.PortableExecutable;
-using System.Text;
 using Microsoft.Build.Framework;
 using Microsoft.Build.Utilities;
 

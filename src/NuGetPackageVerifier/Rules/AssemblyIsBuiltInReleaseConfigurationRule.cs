a491ebb68848f599e5ae9fc23508b82ca67640b4
Ajay Bhargav Baaskaran <ajaybhargavb@gmail.com>
2017-04-27 14:37:04 -0700

    Re-enabling AssemblyIsBuiltInReleaseConfigurationRule (#242)
    * Re-enabling AssemblyIsBuiltInReleaseConfigurationRule

--- src/NuGetPackageVerifier/Rules/AssemblyIsBuiltInReleaseConfigurationRule.cs
+++ src/NuGetPackageVerifier/Rules/AssemblyIsBuiltInReleaseConfigurationRule.cs
@@ -1,45 +1,84 @@
-﻿// Copyright (c) .NET Foundation. All rights reserved.
+// Copyright (c) .NET Foundation. All rights reserved.
 // Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
 
+using System;
 using System.Collections.Generic;
 using System.Diagnostics;
+using System.IO;
 using System.Linq;
-using Mono.Cecil;
-using Mono.Collections.Generic;
-using NuGetPackageVerifier.Logging;
+using System.Reflection.Metadata;
+using System.Reflection.PortableExecutable;
 
 namespace NuGetPackageVerifier.Rules
 {
-    public class AssemblyIsBuiltInReleaseConfigurationRule : AssemblyHasAttributeRuleBase
+    public class AssemblyIsBuiltInReleaseConfigurationRule : IPackageVerifierRule
     {
-        // TODO remove
-        public override IEnumerable<PackageVerifierIssue> Validate(PackageAnalysisContext context)
+        // TODO: Revert to using Mono.Cecil when https://github.com/jbevain/cecil/issues/306 is fixed.
+        public IEnumerable<PackageVerifierIssue> Validate(PackageAnalysisContext context)
         {
-            // todo remove "Cecil throws ArgumentException when evaluating constructor arguments"
-            context.Logger.Log(LogLevel.Info, $"{nameof(AssemblyIsBuiltInReleaseConfigurationRule)} skipped");
-            return Enumerable.Empty<PackageVerifierIssue>();
+            var issues = new List<PackageVerifierIssue>();
+            foreach (var currentFile in context.PackageReader.GetFiles())
+            {
+                var extension = Path.GetExtension(currentFile);
+                if (extension.Equals(".dll", StringComparison.OrdinalIgnoreCase) ||
+                    extension.Equals(".exe", StringComparison.OrdinalIgnoreCase))
+                {
+                    try
+                    {
+                        using (var packageFileStream = context.PackageReader.GetStream(currentFile))
+                        using (var stream = new MemoryStream())
+                        {
+                            // packageFileStream is not a seekable stream. So wrap it in a memory stream for PEReader to consume.
+                            packageFileStream.CopyTo(stream);
+                            stream.Position = 0;
+                            using (var peReader = new PEReader(stream))
+                            {
+                                var reader = peReader.GetMetadataReader();
+                                foreach (var handle in reader.CustomAttributes)
+                                {
+                                    var customAttribute = reader.GetCustomAttribute(handle);
+                                    var ctor = reader.GetMemberReference((MemberReferenceHandle)customAttribute.Constructor);
+                                    var type = reader.GetTypeReference((TypeReferenceHandle)ctor.Parent);
+                                    var typeName = reader.GetString(type.Name);
+                                    if (string.Equals(typeof(DebuggableAttribute).Name, typeName, StringComparison.Ordinal))
+                                    {
+                                        var attribute = customAttribute.DecodeValue(NullProvider.Instance);
+
+                                        var debuggingMode = (DebuggableAttribute.DebuggingModes)attribute.FixedArguments.Single().Value;
+                                        if (debuggingMode.HasFlag(DebuggableAttribute.DebuggingModes.Default) ||
+                                            debuggingMode.HasFlag(DebuggableAttribute.DebuggingModes.DisableOptimizations))
+                                        {
+                                            issues.Add(PackageIssueFactory.AssemblyHasIncorrectBuildConfiguration(currentFile));
+                                        };
+
+                                        break;
+                                    }
+                                }
+                            }
+                        }
+                    }
+                    catch (InvalidOperationException)
+                    {
+                        // This assembly is not managed.
+                    }
+                }
+            }
+
+            return issues;
         }
 
-        public override IEnumerable<PackageVerifierIssue> ValidateAttribute(string currentFilePath, AssemblyDefinition assembly, Collection<CustomAttribute> assemblyAttributes)
+        private class NullProvider : ICustomAttributeTypeProvider<object>
         {
-            if (!HasReleaseConfiguration(assemblyAttributes))
-            {
-                yield return PackageIssueFactory.AssemblyHasIncorrectBuildConfiguration(currentFilePath);
-            }
-        }
+            public static NullProvider Instance { get; } = new NullProvider();
 
-        private static bool HasReleaseConfiguration(Collection<CustomAttribute> assemblyAttributes)
-        {
-            var foundAttr = assemblyAttributes.SingleOrDefault(attr => attr.AttributeType.FullName == typeof(DebuggableAttribute).FullName);
-            if (foundAttr == null)
-            {
-                return false;
-            }
-
-            var foundAttrArg = foundAttr.ConstructorArguments.SingleOrDefault();
-            var attrValue = (DebuggableAttribute.DebuggingModes)foundAttrArg.Value;
-            return !attrValue.HasFlag(DebuggableAttribute.DebuggingModes.Default) &&
-                   !attrValue.HasFlag(DebuggableAttribute.DebuggingModes.DisableOptimizations);
+            public object GetPrimitiveType(PrimitiveTypeCode typeCode) => null;
+            public object GetSystemType() => null;
+            public object GetSZArrayType(object elementType) => null;
+            public object GetTypeFromDefinition(MetadataReader reader, TypeDefinitionHandle handle, byte rawTypeKind) => null;
+            public object GetTypeFromReference(MetadataReader reader, TypeReferenceHandle handle, byte rawTypeKind) => null;
+            public object GetTypeFromSerializedName(string name) => null;
+            public PrimitiveTypeCode GetUnderlyingEnumType(object type) => PrimitiveTypeCode.Int32;
+            public bool IsSystemType(object type) => false;
         }
     }
 }

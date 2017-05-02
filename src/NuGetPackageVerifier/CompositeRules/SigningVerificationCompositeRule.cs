bdf55cbeb6c2348c016256d7fd5f9bf084e49707
Ajay Bhargav Baaskaran <ajbaaska@microsoft.com>
2017-05-01 18:20:27 -0700

    Revert "NPV temporarily targeting netcoreapp1.0 to unblock build"
    This reverts commit a15d6fda2084a35945dda53a8009ce08f2f32e05.

--- src/NuGetPackageVerifier/CompositeRules/SigningVerificationCompositeRule.cs
+++ src/NuGetPackageVerifier/CompositeRules/SigningVerificationCompositeRule.cs
@@ -1,4 +1,4 @@
-// Copyright (c) .NET Foundation. All rights reserved.
+﻿// Copyright (c) .NET Foundation. All rights reserved.
 // Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
 
 using System.Collections.Generic;
@@ -9,7 +9,7 @@ namespace NuGetPackageVerifier.Rules
     {
         private readonly IPackageVerifierRule[] _rules = {
             new AssemblyHasCommitHashAttributeRule(),
-            //new AssemblyIsBuiltInReleaseConfigurationRule(),
+            new AssemblyIsBuiltInReleaseConfigurationRule(),
             new AuthenticodeSigningRule(),
             new PowerShellScriptIsSignedRule(),
             new RequiredNuSpecInfoRule(),
--- src/NuGetPackageVerifier/Rules/AssemblyIsBuiltInReleaseConfigurationRule.cs
+++ src/NuGetPackageVerifier/Rules/AssemblyIsBuiltInReleaseConfigurationRule.cs
@@ -17,68 +17,68 @@ namespace NuGetPackageVerifier.Rules
         public IEnumerable<PackageVerifierIssue> Validate(PackageAnalysisContext context)
         {
             var issues = new List<PackageVerifierIssue>();
-            //foreach (var currentFile in context.PackageReader.GetFiles())
-            //{
-            //    var extension = Path.GetExtension(currentFile);
-            //    if (extension.Equals(".dll", StringComparison.OrdinalIgnoreCase) ||
-            //        extension.Equals(".exe", StringComparison.OrdinalIgnoreCase))
-            //    {
-            //        try
-            //        {
-            //            using (var packageFileStream = context.PackageReader.GetStream(currentFile))
-            //            using (var stream = new MemoryStream())
-            //            {
-            //                // packageFileStream is not a seekable stream. So wrap it in a memory stream for PEReader to consume.
-            //                packageFileStream.CopyTo(stream);
-            //                stream.Position = 0;
-            //                using (var peReader = new PEReader(stream))
-            //                {
-            //                    var reader = peReader.GetMetadataReader();
-            //                    foreach (var handle in reader.CustomAttributes)
-            //                    {
-            //                        var customAttribute = reader.GetCustomAttribute(handle);
-            //                        var ctor = reader.GetMemberReference((MemberReferenceHandle)customAttribute.Constructor);
-            //                        var type = reader.GetTypeReference((TypeReferenceHandle)ctor.Parent);
-            //                        var typeName = reader.GetString(type.Name);
-            //                        if (string.Equals(typeof(DebuggableAttribute).Name, typeName, StringComparison.Ordinal))
-            //                        {
-            //                            var attribute = customAttribute.DecodeValue(NullProvider.Instance);
+            foreach (var currentFile in context.PackageReader.GetFiles())
+            {
+                var extension = Path.GetExtension(currentFile);
+                if (extension.Equals(".dll", StringComparison.OrdinalIgnoreCase) ||
+                    extension.Equals(".exe", StringComparison.OrdinalIgnoreCase))
+                {
+                    try
+                    {
+                        using (var packageFileStream = context.PackageReader.GetStream(currentFile))
+                        using (var stream = new MemoryStream())
+                        {
+                            // packageFileStream is not a seekable stream. So wrap it in a memory stream for PEReader to consume.
+                            packageFileStream.CopyTo(stream);
+                            stream.Position = 0;
+                            using (var peReader = new PEReader(stream))
+                            {
+                                var reader = peReader.GetMetadataReader();
+                                foreach (var handle in reader.CustomAttributes)
+                                {
+                                    var customAttribute = reader.GetCustomAttribute(handle);
+                                    var ctor = reader.GetMemberReference((MemberReferenceHandle)customAttribute.Constructor);
+                                    var type = reader.GetTypeReference((TypeReferenceHandle)ctor.Parent);
+                                    var typeName = reader.GetString(type.Name);
+                                    if (string.Equals(typeof(DebuggableAttribute).Name, typeName, StringComparison.Ordinal))
+                                    {
+                                        var attribute = customAttribute.DecodeValue(NullProvider.Instance);
 
-            //                            var debuggingMode = (DebuggableAttribute.DebuggingModes)attribute.FixedArguments.Single().Value;
-            //                            if (debuggingMode.HasFlag(DebuggableAttribute.DebuggingModes.Default) ||
-            //                                debuggingMode.HasFlag(DebuggableAttribute.DebuggingModes.DisableOptimizations))
-            //                            {
-            //                                issues.Add(PackageIssueFactory.AssemblyHasIncorrectBuildConfiguration(currentFile));
-            //                            };
+                                        var debuggingMode = (DebuggableAttribute.DebuggingModes)attribute.FixedArguments.Single().Value;
+                                        if (debuggingMode.HasFlag(DebuggableAttribute.DebuggingModes.Default) ||
+                                            debuggingMode.HasFlag(DebuggableAttribute.DebuggingModes.DisableOptimizations))
+                                        {
+                                            issues.Add(PackageIssueFactory.AssemblyHasIncorrectBuildConfiguration(currentFile));
+                                        };
 
-            //                            break;
-            //                        }
-            //                    }
-            //                }
-            //            }
-            //        }
-            //        catch (InvalidOperationException)
-            //        {
-            //            // This assembly is not managed.
-            //        }
-            //    }
-            //}
+                                        break;
+                                    }
+                                }
+                            }
+                        }
+                    }
+                    catch (InvalidOperationException)
+                    {
+                        // This assembly is not managed.
+                    }
+                }
+            }
 
             return issues;
         }
 
-        //private class NullProvider : ICustomAttributeTypeProvider<object>
-        //{
-        //    public static NullProvider Instance { get; } = new NullProvider();
+        private class NullProvider : ICustomAttributeTypeProvider<object>
+        {
+            public static NullProvider Instance { get; } = new NullProvider();
 
-        //    public object GetPrimitiveType(PrimitiveTypeCode typeCode) => null;
-        //    public object GetSystemType() => null;
-        //    public object GetSZArrayType(object elementType) => null;
-        //    public object GetTypeFromDefinition(MetadataReader reader, TypeDefinitionHandle handle, byte rawTypeKind) => null;
-        //    public object GetTypeFromReference(MetadataReader reader, TypeReferenceHandle handle, byte rawTypeKind) => null;
-        //    public object GetTypeFromSerializedName(string name) => null;
-        //    public PrimitiveTypeCode GetUnderlyingEnumType(object type) => PrimitiveTypeCode.Int32;
-        //    public bool IsSystemType(object type) => false;
-        //}
+            public object GetPrimitiveType(PrimitiveTypeCode typeCode) => null;
+            public object GetSystemType() => null;
+            public object GetSZArrayType(object elementType) => null;
+            public object GetTypeFromDefinition(MetadataReader reader, TypeDefinitionHandle handle, byte rawTypeKind) => null;
+            public object GetTypeFromReference(MetadataReader reader, TypeReferenceHandle handle, byte rawTypeKind) => null;
+            public object GetTypeFromSerializedName(string name) => null;
+            public PrimitiveTypeCode GetUnderlyingEnumType(object type) => PrimitiveTypeCode.Int32;
+            public bool IsSystemType(object type) => false;
+        }
     }
 }

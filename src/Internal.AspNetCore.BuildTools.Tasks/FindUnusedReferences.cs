da73e105376f5b980593c1519e58d8bcde8fdb65
Doug Bunting <dougbu@microsoft.com>
2017-05-16 10:50:35 -0700

    Get rid of package downgrade warnings - make System.Collections.Immutable match what System.Reflection.Metadata pulls in - make System.Runtime.Serialization.Primitives match what Newtonsoft.Json pulls in - make `FindUnusedReferences` task run only in `dotnet msbuild`
    nit: make Internal.AspNetCore.Sdk.Tasks project more consistent with Internal.AspNetCore.BuildTools.Tasks

--- src/Internal.AspNetCore.BuildTools.Tasks/FindUnusedReferences.cs
+++ src/Internal.AspNetCore.BuildTools.Tasks/FindUnusedReferences.cs
@@ -1,6 +1,7 @@
 ﻿// Copyright (c) .NET Foundation. All rights reserved.
 // Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
 
+#if NETSTANDARD1_6
 using System;
 using System.Collections.Generic;
 using System.IO;
@@ -80,3 +81,4 @@ namespace Microsoft.AspNetCore.BuildTools
         }
     }
 }
+#endif

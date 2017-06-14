845697f6c5cd146fab5c7d4775f546e52a1cd7cb
Kiran Challa <kichalla@microsoft.com>
2017-06-14 05:03:37 -0700

    Revert "Temporary workaround for MissingMethodException to unblock the mirror"
    This reverts commit 83c7516741d02eafc3a32fc56e9f7a5efcadf9bf.

--- src/Microsoft.AspNetCore.BuildTools.ApiCheck/Loader/CoreClrAssemblyLoader.cs
+++ src/Microsoft.AspNetCore.BuildTools.ApiCheck/Loader/CoreClrAssemblyLoader.cs
@@ -80,10 +80,6 @@ namespace ApiCheck
             {
                 // The assembly has already been loaded.
             }
-            catch (MissingMethodException)
-            {
-                // Temporary workaround for a CoreFx issue
-            }
 
             return false;
         }

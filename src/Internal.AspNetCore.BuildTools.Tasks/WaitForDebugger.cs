139f01b0e478b445188305bc673a5031a36b837a
Nate McMaster <nate.mcmaster@microsoft.com>
2017-06-15 17:56:09 -0700

    Add JsonPeek task and make WaitForDebugger cancelable

--- src/Internal.AspNetCore.BuildTools.Tasks/WaitForDebugger.cs
+++ src/Internal.AspNetCore.BuildTools.Tasks/WaitForDebugger.cs
@@ -1,4 +1,4 @@
-﻿// Copyright (c) .NET Foundation. All rights reserved.
+// Copyright (c) .NET Foundation. All rights reserved.
 // Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
 
 using System.Diagnostics;
@@ -9,13 +9,20 @@ using Microsoft.Build.Utilities;
 namespace Microsoft.AspNetCore.BuildTools
 {
 #if SDK
-    public class Sdk_WaitForDebugger : Task
+    public class Sdk_WaitForDebugger : Task, ICancelableTask
 #elif BuildTools
     public class WaitForDebugger : Task
 #else
 #error This must be built either for an SDK or for BuildTools
 #endif
     {
+        private bool _canceled;
+
+        public void Cancel()
+        {
+            _canceled = true;
+        }
+
         public override bool Execute()
         {
             Log.LogMessage(MessageImportance.High, $"Waiting for debugger. Process ID: {Process.GetCurrentProcess().Id}");
@@ -24,13 +31,13 @@ namespace Microsoft.AspNetCore.BuildTools
             var maxTimeout = 30 * 1000;
             var step = 150;
 
-            while (!Debugger.IsAttached && maxTimeout > 0)
+            while (!Debugger.IsAttached && maxTimeout > 0 && !_canceled)
             {
                 Thread.Sleep(step);
                 maxTimeout -= step;
             }
 
-            if (!Debugger.IsAttached)
+            if (!Debugger.IsAttached && !_canceled)
             {
                 Log.LogMessage(MessageImportance.High, "Waiting for debugger timed out. Continuing execution.");
             }
--- test/BuildTools.Tasks.Tests/MockEngine.cs
+++ test/BuildTools.Tasks.Tests/MockEngine.cs
@@ -1,11 +1,11 @@
-﻿// Copyright (c) .NET Foundation. All rights reserved.
+// Copyright (c) .NET Foundation. All rights reserved.
 // Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
 
 using System;
 using System.Collections;
 using System.Collections.Generic;
 using Microsoft.Build.Framework;
-using Xunit;
+using Xunit.Sdk;
 
 namespace BuildTools.Tasks.Tests
 {
@@ -13,10 +13,11 @@ namespace BuildTools.Tasks.Tests
     {
         public ICollection<BuildMessageEventArgs> Messages { get; } = new List<BuildMessageEventArgs>();
         public ICollection<BuildWarningEventArgs> Warnings { get; } = new List<BuildWarningEventArgs>();
+        public ICollection<BuildErrorEventArgs> Errors { get; } = new List<BuildErrorEventArgs>();
 
         public bool IsRunningMultipleNodes => false;
 
-        public bool ContinueOnError => false;
+        public bool ContinueOnError { get; set; }
 
         public int LineNumberOfTaskNode => 0;
 
@@ -32,7 +33,11 @@ namespace BuildTools.Tasks.Tests
 
         public void LogErrorEvent(BuildErrorEventArgs e)
         {
-            Assert.False(true, e.Message);
+            Errors.Add(e);
+            if (!ContinueOnError)
+            {
+                throw new XunitException("Task error: " + e.Message);
+            }
         }
 
         #region NotImplemented

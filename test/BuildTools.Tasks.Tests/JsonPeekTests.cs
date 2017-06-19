e6a804c196472a04a21dba3943f9e89cfbe1f7af
Nate McMaster <natemcmaster@users.noreply.github.com>
2017-06-19 09:31:48 -0700

    Add JsonPeek task and make WaitForDebugger cancelable (#274)

---
+++ test/BuildTools.Tasks.Tests/JsonPeekTests.cs
@@ -0,0 +1,111 @@
+// Copyright (c) .NET Foundation. All rights reserved.
+// Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
+
+using System;
+using System.IO;
+using Microsoft.AspNetCore.BuildTools;
+using Xunit;
+
+namespace BuildTools.Tasks.Tests
+{
+    public class JsonPeekTests
+    {
+        [Fact]
+        public void CanReadJsonFile()
+        {
+            var filepath = Path.Combine(AppContext.BaseDirectory, "Resources", "sampledata.json");
+            var task = new JsonPeek
+            {
+                BuildEngine = new MockEngine(),
+                JsonInputPath = filepath,
+                Query = "$.sdk.version"
+            };
+
+            Assert.True(task.Execute(), "Task should pass");
+            var result = Assert.Single(task.Result);
+            Assert.Equal("1.2.3", result.ItemSpec);
+            Assert.Equal("String", result.GetMetadata("Type"));
+        }
+
+        [Fact]
+        public void CanReadContent()
+        {
+            var filepath = Path.Combine(AppContext.BaseDirectory, "Resources", "sampledata.json");
+            var content = File.ReadAllText(filepath);
+            var task = new JsonPeek
+            {
+                BuildEngine = new MockEngine(),
+                JsonContent = content,
+                Query = "$.runtimes[*].version"
+            };
+
+            Assert.True(task.Execute(), "Task should pass");
+            Assert.Collection(task.Result,
+                i => Assert.Equal("1.0.0-beta", i.ItemSpec),
+                i => Assert.Equal("1.0.0-alpha", i.ItemSpec));
+        }
+
+        [Fact]
+        public void HandlesEmptyQuery()
+        {
+            var task = new JsonPeek
+            {
+                BuildEngine = new MockEngine(),
+                JsonContent = "{}",
+                Query = "$.dne.property.notthere"
+            };
+
+            Assert.True(task.Execute(), "Task should pass");
+            Assert.Empty(task.Result);
+        }
+
+        [Fact]
+        public void HandlesBadQuery()
+        {
+            var engine = new MockEngine { ContinueOnError = true };
+            var task = new JsonPeek
+            {
+                BuildEngine = engine,
+                JsonContent = "{}",
+                Query = "this is bad JSONPath syntax"
+            };
+
+            Assert.False(task.Execute(), "Task should not pass");
+            var error = Assert.Single(engine.Errors);
+            Assert.Contains("Unexpected character while parsing path", error.Message);
+            Assert.NotNull(task.Result);
+        }
+
+        [Fact]
+        public void HandlesBadJson()
+        {
+            var engine = new MockEngine { ContinueOnError = true };
+            var task = new JsonPeek
+            {
+                BuildEngine = engine,
+                JsonContent = "{ obj: }",
+                Query = ""
+            };
+
+            Assert.False(task.Execute(), "Task should not pass");
+            var error = Assert.Single(engine.Errors);
+            Assert.Contains("Unexpected character encountered while parsing value", error.Message);
+            Assert.NotNull(task.Result);
+        }
+
+        [Fact]
+        public void InvalidToSpecifyContentAndPath()
+        {
+            var task = new JsonPeek
+            {
+                BuildEngine = new MockEngine { ContinueOnError = true },
+                JsonInputPath = Path.Combine(AppContext.BaseDirectory, "Resources", "sampledata.json"),
+                JsonContent = "{}",
+                Query = ""
+            };
+
+            Assert.False(task.Execute(), "Task should not pass");
+            Assert.NotNull(task.Result);
+        }
+    }
+}

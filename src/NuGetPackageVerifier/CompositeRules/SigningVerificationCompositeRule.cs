13cb30512bf4f86cc9180ef8d725119489f5e46f
Ajay Bhargav Baaskaran <ajbaaska@microsoft.com>
2017-07-07 13:53:15 -0700

    Remove DefaultCompositeRule from SigningVerificationCompositeRule

--- src/NuGetPackageVerifier/CompositeRules/SigningVerificationCompositeRule.cs
+++ src/NuGetPackageVerifier/CompositeRules/SigningVerificationCompositeRule.cs
@@ -1,4 +1,4 @@
-﻿// Copyright (c) .NET Foundation. All rights reserved.
+// Copyright (c) .NET Foundation. All rights reserved.
 // Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
 
 using System.Collections.Generic;
@@ -13,7 +13,6 @@ namespace NuGetPackageVerifier.Rules
             new AuthenticodeSigningRule(),
             new PowerShellScriptIsSignedRule(),
             new PackageOwnershipRule(),
-            new DefaultCompositeRule(),
             new PrereleaseDependenciesVersionRule(),
         };
 

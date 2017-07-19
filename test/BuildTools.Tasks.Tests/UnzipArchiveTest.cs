44aba9a0cb746e3f14e573e40d4e2b8ffc3a3cf6
Ryan Brandenburg <rybrande@microsoft.com>
2017-07-19 11:48:24 -0700

    Add Overwrite property to UnzipArchive

--- test/BuildTools.Tasks.Tests/UnzipArchiveTest.cs
+++ test/BuildTools.Tasks.Tests/UnzipArchiveTest.cs
@@ -1,4 +1,4 @@
-﻿// Copyright (c) .NET Foundation. All rights reserved.
+// Copyright (c) .NET Foundation. All rights reserved.
 // Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
 
 using System;
@@ -40,7 +40,7 @@ namespace BuildTools.Tasks.Tests
                 BuildEngine = new MockEngine(),
             };
 
-            Assert.True(task.Execute(), "Task failed");
+            Assert.True(task.Execute(), "The task failed but should have passed.");
             Assert.True(Directory.Exists(outDir), outDir + " does not exist");
             Assert.Equal(files.Length, task.OutputFiles.Length);
 
@@ -52,6 +52,67 @@ namespace BuildTools.Tasks.Tests
                 var outFile = Path.Combine(outDir, file);
                 Assert.True(File.Exists(outFile), outFile + " does not exist");
             }
+        }
+
+        [Fact]
+        public void Overwrites()
+        {
+            var files = new[]
+            {
+                "a.txt",
+                "dir/b.txt"
+            };
+
+            var dest = CreateZip(files);
+            var outDir = Path.Combine(_tempDir, "out");
+
+            var task = new UnzipArchive
+            {
+                File = dest,
+                Destination = outDir,
+                BuildEngine = new MockEngine(),
+                Overwrite = true
+            };
+
+            Directory.CreateDirectory(outDir);
+
+            // Create a.txt before trying to unzip
+            var path = Path.Combine(outDir, "a.txt");
+            File.WriteAllText(path, "contents!");
+            Assert.True(task.Execute(), "The task failed but should have passed.");
+            Assert.Empty(File.ReadAllText(path));
+        }
+
+        [Fact]
+        public void DoesNotOverwrite()
+        {
+            var files = new[]
+            {
+                "a.txt",
+                "dir/b.txt"
+            };
+
+            var dest = CreateZip(files);
+            var outDir = Path.Combine(_tempDir, "out");
+
+            var task = new UnzipArchive
+            {
+                File = dest,
+                Destination = outDir,
+                BuildEngine = new MockEngine(),
+                Overwrite = false
+            };
+
+            Directory.CreateDirectory(outDir);
+
+            // Create a.txt before trying to unzip
+            var path = Path.Combine(outDir, "a.txt");
+            var contents = "contents!";
+            File.WriteAllText(path, contents);
+
+            Assert.Throws<IOException>(() => task.Execute());
+
+            Assert.Equal(contents, File.ReadAllText(path));
         }
 
         private string CreateZip(string[] files)

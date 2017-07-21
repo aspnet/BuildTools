a69ee8c39f57c40caffa2b98dd7cbce2e92373c7
Pranav K <prkrishn@hotmail.com>
2017-07-21 16:01:42 -0700

    Revert "Skip PrereleaseDependenciesVersionRule until we have RTM dependencies"
    This reverts commit c525f5d7061a5636f47f6e00cb3142796aeb944c.

--- src/NuGetPackageVerifier/CompositeRules/SigningVerificationCompositeRule.cs
+++ src/NuGetPackageVerifier/CompositeRules/SigningVerificationCompositeRule.cs
@@ -1,4 +1,4 @@
-// Copyright (c) .NET Foundation. All rights reserved.
+﻿// Copyright (c) .NET Foundation. All rights reserved.
 // Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
 
 using System.Collections.Generic;
@@ -13,6 +13,7 @@ namespace NuGetPackageVerifier.Rules
             new AuthenticodeSigningRule(),
             new PowerShellScriptIsSignedRule(),
             new PackageOwnershipRule()
+            new PrereleaseDependenciesVersionRule(),
         };
 
         public IEnumerable<PackageVerifierIssue> Validate(PackageAnalysisContext context)

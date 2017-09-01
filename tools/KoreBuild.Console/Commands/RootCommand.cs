e6c5ade169c63f414bdd46e70cd198b7d3c19fa5
Ryan Brandenburg <rybrande@microsoft.com>
2017-09-01 11:11:54 -0700

    Docker needs more work

--- tools/KoreBuild.Console/Commands/RootCommand.cs
+++ tools/KoreBuild.Console/Commands/RootCommand.cs
@@ -12,7 +12,6 @@ namespace KoreBuild.Console.Commands
         {
             application.FullName = "korebuild";
 
-            application.Command("docker-build", new DockerBuildCommand().Configure, throwOnUnexpectedArg:false);
             application.Command("install-tools", new InstallToolsCommand().Configure, throwOnUnexpectedArg:false);
             application.Command("msbuild", new MSBuildCommand().Configure, throwOnUnexpectedArg:false);
 

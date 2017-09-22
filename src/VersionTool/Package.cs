9da945d42ccf3a4f87538597e40719dbd020640e
Nate McMaster <nate.mcmaster@microsoft.com>
2017-09-22 13:40:54 -0700

    Delete VersionTool

--- src/VersionTool/Package.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace VersionTool
-{
-    public class Package
-    {
-        public string Name { get; set; }
-        public string CurrentVersion { get; set; }
-        public string NewVersion { get; set; }
-    }
-}

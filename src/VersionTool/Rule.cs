5abefb5995a953a66021202f7186b8e7654bacf5
Nate McMaster <natemcmaster@users.noreply.github.com>
2017-09-22 13:53:01 -0700

    Delete VersionTool (#411)

--- src/VersionTool/Rule.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace VersionTool
-{
-    public class Rule
-    {
-        public string Pattern { get; set; }
-        public string CurrentVersion { get; set; }
-        public string NewVersion { get; set; }
-    }
-}

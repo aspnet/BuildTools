75de104c1bc6f532a6b00591ad4111367c33cace
Ryan Brandenburg <rybrande@microsoft.com>
2017-10-10 11:04:43 -0700

    Use SourceLink

---
+++ test/BuildTools.Tasks.Tests/CreateSourceLinkTest.cs
@@ -0,0 +1,100 @@
+// Copyright (c) .NET Foundation. All rights reserved.
+// Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
+
+using System;
+using System.IO;
+using Microsoft.AspNetCore.BuildTools;
+using Microsoft.DotNet.PlatformAbstractions;
+using Xunit;
+
+namespace BuildTools.Tasks.Tests
+{
+    public class CreateSourceLinkTest
+    {
+        [Fact]
+        public void CreatesFile()
+        {
+            var destination = "sourcelink.json";
+
+            var repoName = "aspnet/BuildTools";
+
+            var originUrl = $"git@github.com:{repoName}.git";
+            var commit = "5153bbdfa98dcc27a61d591ce09a0d632875e66f";
+            var rootDir = GetRootDirectory();
+
+            var task = new CreateSourceLink
+            {
+                OriginUrl = originUrl,
+                Commit = commit,
+                DestinationFile = destination,
+                RootDirectory = rootDir
+            };
+
+            try
+            {
+                Assert.True(task.Execute(), "The task failed but should have passed.");
+                Assert.True(File.Exists(destination), "SourceLink file doesn't exist.");
+
+                var expectedUrl = $"https://raw.githubusercontent.com/{repoName}/{commit}/*";
+                var expected = $"{{\"documents\":{{\"{rootDir}*\":\"{expectedUrl}\"}}}}";
+
+                var resultText = File.ReadAllText(destination);
+
+                Assert.Equal(expected, resultText);
+            }
+            finally
+            {
+                File.Delete(destination);
+            }
+        }
+
+        [Fact]
+        public void HandlesHttps()
+        {
+            var destination = "sourcelink.json";
+
+            var repoName = "aspnet/BuildTools";
+
+            var originUrl = $"https://github.com/{repoName}.git";
+            var commit = "5153bbdfa98dcc27a61d591ce09a0d632875e66f";
+            var rootDir = GetRootDirectory();
+
+            var task = new CreateSourceLink
+            {
+                OriginUrl = originUrl,
+                Commit = commit,
+                DestinationFile = destination,
+                RootDirectory = rootDir
+            };
+
+            try
+            {
+                Assert.True(task.Execute(), "The task failed but should have passed.");
+                Assert.True(File.Exists(destination), "SourceLink file doesn't exist.");
+
+                var expectedUrl = $"https://raw.githubusercontent.com/{repoName}/{commit}/*";
+                var expected = $"{{\"documents\":{{\"{rootDir}*\":\"{expectedUrl}\"}}}}";
+
+                Assert.Equal(expected, File.ReadAllText(destination));
+            }
+            finally
+            {
+                File.Delete(destination);
+            }
+        }
+
+        private static string GetRootDirectory()
+        {
+            switch(RuntimeEnvironment.OperatingSystemPlatform)
+            {
+                case Platform.Windows:
+                    return "C:/";
+                case Platform.Linux:
+                case Platform.Darwin:
+                    return "/home/";
+                default:
+                    throw new NotImplementedException($"SourceLink tests don't yet support {RuntimeEnvironment.OperatingSystemPlatform}.");
+            }
+        }
+    }
+}

aa6d3b038d01fb3a64f4b7ea3e55f281b6eda9ea
Nate McMaster <nate.mcmaster@microsoft.com>
2017-11-08 13:08:15 -0800

    Add the Run and RunDotNet tasks so test output won't be captured by the MSBuild console logger

---
+++ modules/BuildTools.Tasks/Run.cs
@@ -0,0 +1,29 @@
+// Copyright (c) .NET Foundation. All rights reserved.
+// Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
+
+using Microsoft.Build.Framework;
+
+namespace Microsoft.AspNetCore.BuildTools
+{
+    /// <summary>
+    /// A task that runs a process without piping output into the logger.
+    /// See <seealso cref="RunBase" /> for more arguments.
+    /// </summary>
+#if SDK
+    public class Sdk_Run
+#elif BuildTools
+    public class Run
+#else
+#error This must be built either for an SDK or for BuildTools
+#endif
+        : RunBase
+    {
+        /// <summary>
+        /// The executable to run. Can be a file path or a command for an executable on the system PATH.
+        /// </summary>
+        [Required]
+        public string FileName { get; set; }
+
+        protected override string GetExecutable() => FileName;
+    }
+}
---
+++ modules/BuildTools.Tasks/RunDotNet.cs
@@ -0,0 +1,28 @@
+// Copyright (c) .NET Foundation. All rights reserved.
+// Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
+
+using Microsoft.Extensions.CommandLineUtils;
+
+namespace Microsoft.AspNetCore.BuildTools
+{
+    /// <summary>
+    /// A task that runs a dotnet command without piping output into the logger.
+    /// See <seealso cref="RunBase" /> for more arguments.
+    /// </summary>
+#if SDK
+    public class Sdk_RunDotNet
+#elif BuildTools
+    public class RunDotNet
+#else
+#error This must be built either for an SDK or for BuildTools
+#endif
+        : RunBase
+    {
+        protected override string GetExecutable()
+#if NET46
+            => "dotnet";
+#else
+            => DotNetMuxer.MuxerPathOrDefault();
+#endif
+    }
+}

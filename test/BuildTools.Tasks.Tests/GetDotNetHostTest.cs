1f9404c45433cb27b82e54bde21198ea4713219f
Nate McMaster <nate.mcmaster@microsoft.com>
2017-11-20 11:23:12 -0800

    Remove dependency on Microsoft.DotNet.Cli.Utils

--- test/BuildTools.Tasks.Tests/GetDotNetHostTest.cs
+++ test/BuildTools.Tasks.Tests/GetDotNetHostTest.cs
@@ -1,9 +1,9 @@
 ﻿// Copyright (c) .NET Foundation. All rights reserved.
 // Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
 
+using System.Diagnostics;
 using System.IO;
 using Microsoft.AspNetCore.BuildTools;
-using Microsoft.DotNet.Cli.Utils;
 using Xunit;
 
 namespace BuildTools.Tasks.Tests
@@ -19,7 +19,7 @@ namespace BuildTools.Tasks.Tests
             };
 
             Assert.True(task.Execute(), "Task failed");
-            var muxer = new Muxer().MuxerPath;
+            var muxer = Process.GetCurrentProcess().MainModule.FileName;
             Assert.Equal(muxer, task.ExecutablePath);
             Assert.Equal(Path.GetDirectoryName(muxer) + Path.DirectorySeparatorChar, task.DotNetDirectory);
         }

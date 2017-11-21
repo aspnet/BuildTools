925a2215bcd5202622c2c72d24103eac51c9fe37
Nate McMaster <natemcmaster@users.noreply.github.com>
2017-11-21 11:39:11 -0800

    Support publishing assets and skipping tests for orchestrated build (#477)

--- push.ps1
+++ push.ps1
@@ -2,8 +2,18 @@
 #requires -version 4
 [cmdletbinding(SupportsShouldProcess = $true)]
 param(
+    [ValidateSet('default', 'blob')]
+    [string]$PublishType = 'default',
+
+    # used for PublishType = default
     [string]$NuGetFeed = 'https://dotnet.myget.org/F/aspnetcore-tools/api/v2/package',
-    [string]$AzureStorageAccount = 'aspnetcore'
+    [string]$AzureStorageAccount = 'aspnetcore',
+
+    # used for PublishType = blob
+    [string]$PublishBlobFeedUrl,
+    [string]$PublishBlobFeedKey,
+    [Alias('d')]
+    [string]$DotNetHome = $null
 )
 
 $ErrorActionPreference = 'Stop'
@@ -14,20 +24,44 @@ if ($env:BUILD_IS_PERSONAL) {
     Write-Host -ForegroundColor Yellow 'Automatically setting -WhatIf for personal builds'
 }
 
-Import-Module -Force -Scope Local $PSScriptRoot/files/KoreBuild/KoreBuild.psd1
-Import-Module -Force -Scope Local $PSScriptRoot/scripts/PushNuGet.psm1
+if (!$DotNetHome) {
+    $DotNetHome = if ($env:DOTNET_HOME) { $env:DOTNET_HOME } `
+        elseif ($env:USERPROFILE) { Join-Path $env:USERPROFILE '.dotnet'} `
+        elseif ($env:HOME) {Join-Path $env:HOME '.dotnet'}`
+        else { Join-Path $PSScriptRoot '.dotnet'}
+}
 
-$artifacts = Join-Path $PSScriptRoot 'artifacts'
+$ToolsSource = "https://$AzureStorageAccount.blob.core.windows.net/buildtools"
+$ConfigFile = Join-Path $PSScriptRoot 'korebuild.json'
 
-Get-ChildItem "$artifacts/build/*.nupkg" | Push-NuGetPackage -Feed $NuGetFeed -ApiKey $env:APIKEY -WhatIf:$WhatIfPreference
+# The default publish type for builds on aspnetci
+if ($PublishType -eq 'default') {
+    Import-Module -Force -Scope Local $PSScriptRoot/files/KoreBuild/KoreBuild.psd1
+    Import-Module -Force -Scope Local $PSScriptRoot/scripts/PushNuGet.psm1
 
-$settings = [xml] (Get-Content (Join-Path $PSScriptRoot 'version.props'))
-$channelName = $settings.Project.PropertyGroup.KoreBuildChannel
+    $artifacts = Join-Path $PSScriptRoot 'artifacts'
 
-Write-Host "Pushing azure artifacts for '$channelName' channel"
+    Get-ChildItem "$artifacts/build/*.nupkg" | Push-NuGetPackage -Feed $NuGetFeed -ApiKey $env:APIKEY -WhatIf:$WhatIfPreference
 
-& "$PSScriptRoot/scripts/UploadBlobs.ps1" `
-    -Channel $channelName `
-    -ArtifactsDir $artifacts `
-    -AzureStorageAccount $AzureStorageAccount `
-    -WhatIf:$WhatIfPreference
+    $settings = [xml] (Get-Content (Join-Path $PSScriptRoot 'version.props'))
+    $channelName = $settings.Project.PropertyGroup.KoreBuildChannel
+
+    Write-Host "Pushing azure artifacts for '$channelName' channel"
+
+    & "$PSScriptRoot/scripts/UploadBlobs.ps1" `
+        -Channel $channelName `
+        -ArtifactsDir $artifacts `
+        -AzureStorageAccount $AzureStorageAccount `
+        -WhatIf:$WhatIfPreference
+}
+# Publishing for orchestrated builds
+elseif ($PublishType -eq 'blob') {
+    # Requires that you first build korebuild locally
+    Import-Module -Force -Scope Local $PSScriptRoot/obj/korebuild/KoreBuild.psd1
+
+    Set-KoreBuildSettings -ToolsSource $ToolsSource -DotNetHome $DotNetHome -RepoPath $PSScriptRoot -ConfigFile $ConfigFile
+    Invoke-KoreBuildCommand 'default-build' '-t:Publish' "-p:PublishBlobFeedUrl=$PublishBlobFeedUrl" "-p:PublishBlobFeedKey=$PublishBlobFeedKey" '-bl:artifacts/msbuild/push.binlog'
+}
+else {
+    throw "PublishType $PublishType not supported"
+}

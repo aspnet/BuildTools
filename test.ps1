35708da3c1a5c9dd5107cda239b8c9e6324155f1
Ryan Brandenburg <rybrande@microsoft.com>
2017-12-22 14:25:40 -0800

    Update ApiCheck generation and BuildTools testing script

--- test.ps1
+++ test.ps1
@@ -2,9 +2,9 @@
 #requires -version 4
 [CmdletBinding(PositionalBinding = $true)]
 param(
-    [Parameter(Mandatory=$true)]
+    [Parameter(Mandatory = $true)]
     [string]$Command,
-    [Parameter(Mandatory=$true)]
+    [Parameter(Mandatory = $true)]
     [string]$RepoPath,
     [switch]$NoBuild = $false,
     [Parameter(ValueFromRemainingArguments = $true)]
@@ -12,9 +12,27 @@ param(
 )
 
 if (!$NoBuild) {
-    & .\build.ps1 /t:PackageKoreBuild
+    & .\build.ps1 /p:SkipTests=$true
 }
 
 $toolsSource = "$PSScriptRoot/artifacts/"
+$latestFile = Join-Path $toolsSource "korebuild/channels/dev/latest.txt"
+$toolsVersion = $null
+foreach ($line in Get-Content $latestFile) {
+    $toolsVersion = $line.Split(":")[1]
+    break
+}
+$packageDir = Join-Path $toolsSource "build\"
+$versionPropsPath = Join-Path $toolsSource "dotnetpackageversion.props"
+$sourcePropsPath = Join-Path $toolsSource "source.props"
+
+$versionPropsValue = "<Project><PropertyGroup><InternalAspNetCoreSdkPackageVersion>$toolsVersion</InternalAspNetCoreSdkPackageVersion></PropertyGroup></Project>"
+$sourcePropsValue = "<Project><PropertyGroup><DotNetRestoreSources>$packageDir</DotNetRestoreSources></PropertyGroup></Project>"
+
+Out-File -FilePath $versionPropsPath -InputObject $versionPropsValue
+Out-File -FilePath $sourcePropsPath -InputObject $sourcePropsValue
+
+$Arguments += "/p:DotNetPackageVersionPropsPath=$versionPropsPath"
+$Arguments += "/p:DotNetRestoreSourcePropsPath=$sourcePropsPath"
 
 & .\scripts\bootstrapper\run.ps1 -Update -Reinstall -Command $Command -Path $RepoPath -ToolsSource $toolsSource @Arguments

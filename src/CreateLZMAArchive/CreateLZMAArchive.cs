c67923b7d7555d9b6326c220dfbb843ec4c54b2e
John Luo <johluo@microsoft.com>
2018-02-26 17:55:05 -0800

    Task package format

---
+++ src/CreateLZMAArchive/CreateLZMAArchive.cs
@@ -0,0 +1,42 @@
+// Copyright (c) .NET Foundation and contributors. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+using System.IO;
+using Microsoft.Build.Framework;
+using Microsoft.DotNet.Archive;
+using MSBuildTask = Microsoft.Build.Utilities.Task;
+
+namespace Microsoft.AspNetCore.BuildTools.LZMAArchive
+{
+    public class CreateLZMAArchive : MSBuildTask
+    {
+        [Required]
+        public string OutputArchiveFilePath { get; set; }
+
+        [Required]
+        public string[] ArchiveSources { get; set; }
+
+        public override bool Execute()
+        {
+            var progress = new ConsoleProgressReport();
+            var archive = new IndexedArchive();
+
+            foreach (var source in ArchiveSources)
+            {
+                if (Directory.Exists(source))
+                {
+                    archive.AddDirectory(source, progress);
+                }
+                else
+                {
+                    archive.AddFile(source, Path.GetFileName(source));
+                }
+            }
+
+            archive.Save(OutputArchiveFilePath, progress);
+
+
+            return true;
+        }
+    }
+}

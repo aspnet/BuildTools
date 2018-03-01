599e691c41f502ed9e062b1822ce13b673fc916e
Nate McMaster <natemcmaster@users.noreply.github.com>
2018-03-01 09:53:44 -0800

    Remove bill of materials tasks (#595)
    In the end, this build manifest output didn't prove to be useful enough to keep.
    The .NET Core engineering team created something similar for  the
    Product Construction Plan which was sufficient,
    so we decided to eliminate this altogether
    in favor of consolidating build infrastructure.

--- test/KoreBuild.Tasks.Tests/ChecksumTests.cs
+++ test/KoreBuild.Tasks.Tests/ChecksumTests.cs
@@ -29,24 +29,6 @@ namespace KoreBuild.Tasks.Tests
         }
 
         [Theory]
-        [InlineData("SHA256", "BCFAF334240356E1B97824A866F643B1ADA3C16AA0B5B2BFA8390D8BB54A244C")]
-        [InlineData("SHA384", "5520B01FDE8A8A7EA38DCADFBF3CFAB2818FA0D5A8A16CB11A2FC7F5C9F1497F7B3C528FDB8CE10AA293A4E5FF32297F")]
-        [InlineData("SHA512", "7774962C97EAC52B45291E1410F06AC6EFF6AF9ED38A57E2CEB720650282E46CFE512FAAD68AD9C45B74ED1B7E460198E0B00D5C9EF0404FF76B12F8AD2D329F")]
-        public void ComputesFileChecksumInGroup(string algoritm, string hash)
-        {
-            var task = new ComputeManyChecksum
-            {
-                Items = new[] { new TaskItem(Path.Combine(AppContext.BaseDirectory, "TestResources", "lorem.bin")) },
-                BuildEngine = new MockEngine(),
-                Algorithm = algoritm,
-            };
-
-            Assert.True(task.Execute(), "Task should pass");
-            var item = Assert.Single(task.Items);
-            Assert.Equal(hash, item.GetMetadata("FileHash"));
-        }
-
-        [Theory]
         [InlineData("SHA256", "C442A45BB8D0938AFB2B5B0AA61C3ADA1B346F668A42879B1E653042433FAFCB")]
         [InlineData("SHA384", "F79223FF5E4A392AA01EC8BDF825C3B7F7941F9C5F7CF2A11BC61A8A5D0AF8182BAFC3FBFDACD83AE7A8A8EDF10B0255")]
         [InlineData("SHA512", "F923D2DA8F21B67FF4040FE9C5D00B0E891064E7B1DE47B54C9DA86DAAF215EFC64E282056027BEC2E75A83DE9FA6FFE6CA60F0141E19254B25CAE79C2694777")]

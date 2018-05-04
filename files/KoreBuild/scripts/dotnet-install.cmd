6ddf5108a288b0eb249f257f00f4b28d5558b04a
Liqueur Librazy <im@librazy.org>
2018-05-04 21:39:54 +0000

    Workaround for aspnet/BuildTools#666

--- files/KoreBuild/scripts/dotnet-install.cmd
+++ files/KoreBuild/scripts/dotnet-install.cmd
@@ -1,2 +1,3 @@
-@ECHO OFF
-PowerShell -NoProfile -NoLogo -ExecutionPolicy unrestricted -Command "[System.Threading.Thread]::CurrentThread.CurrentCulture = ''; [System.Threading.Thread]::CurrentThread.CurrentUICulture = '';& '%~dp0dotnet-install.ps1' %*; exit $LASTEXITCODE"
+SET Args=%*
+SET Args=%Args:"='%
+PowerShell -NoProfile -NoLogo -ExecutionPolicy unrestricted -Command "[System.Threading.Thread]::CurrentThread.CurrentCulture = ''; [System.Threading.Thread]::CurrentThread.CurrentUICulture = '';& '%~dp0dotnet-install.ps1' %Args%; exit $LASTEXITCODE"

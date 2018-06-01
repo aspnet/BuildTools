35ac64ee0a51a5fc3c127ba01b19228cf2c1a6a6
Liqueur Librazy <im@librazy.org>
2018-06-01 02:56:49 +0000

    @ECHO OFF

--- files/KoreBuild/scripts/dotnet-install.cmd
+++ files/KoreBuild/scripts/dotnet-install.cmd
@@ -1,3 +1,4 @@
+@ECHO OFF
 SET Args=%*
 SET Args=%Args:"='%
 PowerShell -NoProfile -NoLogo -ExecutionPolicy unrestricted -Command "[System.Threading.Thread]::CurrentThread.CurrentCulture = ''; [System.Threading.Thread]::CurrentThread.CurrentUICulture = '';& '%~dp0dotnet-install.ps1' %Args%; exit $LASTEXITCODE"
